5889774a2c66b2d5dcfad3ba01cf05725e9efd24
Administrator <Administrator@stu1003>
2018-10-11 16:39:52 +0800

    增加管理员dao和管理员dao的实现类

--- src/com/ros/dao/ManagerDao.java
+++ src/com/ros/dao/ManagerDao.java
@@ -3,6 +3,6 @@ package com.ros.dao;
 import com.ros.entity.Manager;
 
 public interface ManagerDao {
-	//管理员登陆验证的查询
+	//管理员登陆验证的查询.
 	Manager queryManagerByNameAndPwd(String adminName,String adminPwd);
 }
--- src/com/ros/dao_impl/ManagerDaoImpl.java
+++ src/com/ros/dao_impl/ManagerDaoImpl.java
@@ -4,7 +4,7 @@ import com.ros.dao.ManagerDao;
 import com.ros.entity.Manager;
 
 public class ManagerDaoImpl implements ManagerDao {
-
+	//
 	@Override
 	public Manager queryManagerByNameAndPwd(String adminName, String adminPwd) {
 		// TODO Auto-generated method stub
