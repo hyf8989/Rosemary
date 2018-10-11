2d8e463fab5a952fd48880f83685353d89205b15
Administrator <Administrator@stu1003>
2018-10-11 17:09:57 +0800

    提交了后台JSP页面的index页面。

---
+++ WebContent/rosemary原型/rosemary/index/js/main.js
@@ -0,0 +1,249 @@
+(function($) {
+    "use strict";
+
+    /*----------------------------
+     jQuery MeanMenu
+    ------------------------------ */
+    jQuery('nav#dropdown').meanmenu();
+
+    /*----------------------------
+     wow js active
+    ------------------------------ */
+    new WOW().init();
+
+    /*----------------------------
+     owl active
+    ------------------------------ */
+    $("#owl-demo").owlCarousel({
+        autoPlay: false,
+        slideSpeed: 2000,
+        pagination: false,
+        navigation: true,
+        items: 4,
+        /* transitionStyle : "fade", */
+        /* [This code for animation ] */
+        navigationText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+        itemsDesktop: [1199, 4],
+        itemsDesktopSmall: [980, 3],
+        itemsTablet: [768, 2],
+        itemsMobile: [479, 1],
+    });
+
+
+    /*----------------------------
+    	slider
+    ------------------------------ */
+    $('#mainslider').nivoSlider({
+        effect: 'random',
+        slices: 15,
+        boxCols: 12,
+        boxRows: 4,
+        animSpeed: 600,
+        pauseTime: 5000,
+        startSlide: 0,
+        controlNav: false,
+        controlNavThumbs: false,
+        pauseOnHover: false,
+        manualAdvance: false,
+        prevText: '<i class="fa fa-chevron-left nivo-prev-icon"></i>',
+        nextText: '<i class="fa fa-chevron-right nivo-next-icon"></i>'
+
+    });
+
+    /*---------------------
+     countdown
+    --------------------- */
+    $('[data-countdown]').each(function() {
+        var $this = $(this),
+            finalDate = $(this).data('countdown');
+        $this.countdown(finalDate, function(event) {
+            $this.html(event.strftime('<span class="cdown days"><span class="time-count">%-D</span> <p>Days</p></span> <span class="cdown hour"><span class="time-count">%-H</span> <p>Hour</p></span> <span class="cdown minutes"><span class="time-count">%M</span> <p>Min</p></span> <span class="cdown second"> <span><span class="time-count">%S</span> <p>Sec</p></span>'));
+        });
+    });
+
+    /*------------------------------
+     feature-owl
+    ------------------------------ */
+    $(".tab-carousel").owlCarousel({
+        autoPlay: false,
+        slideSpeed: 2000,
+        pagination: false,
+        navigation: true,
+        items: 4,
+        /* transitionStyle : "fade", */
+        /* [This code for animation ] */
+        navigationText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+        itemsDesktop: [1199, 4],
+        itemsDesktopSmall: [980, 2],
+        itemsTablet: [768, 1],
+        itemsMobile: [479, 1],
+    });
+
+    /*------------------------------
+     new owl
+    ------------------------------ */
+    $(".new-carousel").owlCarousel({
+        autoPlay: false,
+        slideSpeed: 2000,
+        pagination: false,
+        navigation: true,
+        items: 4,
+        /* transitionStyle : "fade", */
+        /* [This code for animation ] */
+        navigationText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+        itemsDesktop: [1199, 4],
+        itemsDesktopSmall: [980, 2],
+        itemsTablet: [768, 1],
+        itemsMobile: [479, 1],
+    });
+
+    /*------------------------------
+    best owl
+    ------------------------------ */
+    $(".best-carousel").owlCarousel({
+        autoPlay: false,
+        slideSpeed: 1000000,
+        pagination: false,
+        navigation: true,
+        items: 1,
+        /* transitionStyle : "fade", */
+        /* [This code for animation ] */
+        navigationText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+        itemsDesktop: [1199, 1],
+        itemsDesktopSmall: [980, 1],
+        itemsTablet: [768, 1],
+        itemsMobile: [479, 1],
+    });
+
+    /*------------------------------
+    exclusive owl
+    ------------------------------ */
+    $(".exclusive-carousel").owlCarousel({
+        autoPlay: false,
+        slideSpeed: 2000,
+        pagination: false,
+        navigation: true,
+        items: 3,
+        /* transitionStyle : "fade", */
+        /* [This code for animation ] */
+        navigationText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+        itemsDesktop: [1199, 3],
+        itemsDesktopSmall: [980, 2],
+        itemsTablet: [768, 1],
+        itemsMobile: [479, 1],
+    });
+
+    /*------------------------------
+    exclusive owl
+    ------------------------------ */
+    $(".blog-carousel").owlCarousel({
+        autoPlay: false,
+        slideSpeed: 2000,
+        pagination: false,
+        navigation: true,
+        items: 3,
+        /* transitionStyle : "fade", */
+        /* [This code for animation ] */
+        navigationText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+        itemsDesktop: [1199, 3],
+        itemsDesktopSmall: [980, 3],
+        itemsTablet: [768, 2],
+        itemsMobile: [479, 1],
+    });
+
+    /*------------------------------
+    brand-carousel
+    ------------------------------ */
+    $(".brand-carousel").owlCarousel({
+        autoPlay: false,
+        slideSpeed: 2000,
+        pagination: false,
+        navigation: true,
+        items: 6,
+        /* transitionStyle : "fade", */
+        /* [This code for animation ] */
+        navigationText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+        itemsDesktop: [1199, 6],
+        itemsDesktopSmall: [980, 5],
+        itemsTablet: [768, 4],
+        itemsMobile: [479, 3],
+    });
+
+
+    /*------------------------------
+    related owl
+    ------------------------------ */
+    $(".related-carousel").owlCarousel({
+        autoPlay: false,
+        slideSpeed: 2000,
+        pagination: false,
+        navigation: true,
+        items: 4,
+        /* transitionStyle : "fade", */
+        /* [This code for animation ] */
+        navigationText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+        itemsDesktop: [1199, 4],
+        itemsDesktopSmall: [980, 2],
+        itemsTablet: [768, 2],
+        itemsMobile: [479, 1],
+    });
+
+    /*----------------------------
+     scrollUp
+    ---------------------------- */
+    $.scrollUp({
+        scrollText: '<i class="fa fa-arrow-up" aria-hidden="true"></i>',
+        easingType: 'linear',
+        scrollSpeed: 900,
+        animation: 'fade'
+    });
+
+    /*-----------------
+    meanmenu 
+    -----------------*/
+    $('nav#mobile_menu_active').meanmenu({
+        meanScreenWidth: "991",
+        meanMenuContainer: '.mobile-menu-area .container',
+    });
+
+    /*-----------------------
+    cart-plus-minus-button 
+    -------------------------*/
+    $(".cart-plus-minus").append('<div class="dec qtybutton">-</div><div class="inc qtybutton">+</div>');
+    $(".qtybutton").on("click", function() {
+        var $button = $(this);
+        var oldValue = $button.parent().find("input").val();
+        if ($button.text() == "+") {
+            var newVal = parseFloat(oldValue) + 1;
+        } else {
+            // Don't allow decrementing below zero
+            if (oldValue > 0) {
+                var newVal = parseFloat(oldValue) - 1;
+            } else {
+                newVal = 0;
+            }
+        }
+        $button.parent().find("input").val(newVal);
+    });
+
+    /*----------------------------
+     price-slider active
+    -----------------------------*/
+    $("#slider-range").slider({
+        range: true,
+        min: 40,
+        max: 600,
+        values: [87, 475],
+        slide: function(event, ui) {
+            $("#amount").val("£" + ui.values[0] + " - £" + ui.values[1]);
+        }
+    });
+    $("#amount").val("£" + $("#slider-range").slider("values", 0) +
+        " - £" + $("#slider-range").slider("values", 1));
+
+    /*----------------------------
+
+    -----------------------------*/
+
+
+})(jQuery);
