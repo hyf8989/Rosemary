04d092d0a95156c3df6909b0c19e0ae0a75275ab
Administrator <Administrator@stu1003>
2018-10-11 15:41:32 +0800

    上传了一个TEST类

---
+++ src/com/ros/entity/Test.java
@@ -0,0 +1,28 @@
+        /** 
+	 * Project Name:Rosemary 
+	 * File Name:Test.java 
+	 * Package Name:com.ros.entity 
+	 * Date:2018年10月11日下午3:40:29 
+	 * Copyright (c) 2018, ***@126.com All Rights Reserved. 
+	 * 
+	 */
+
+package com.ros.entity;
+
+        /** 
+	 * ClassName: Test <br/> 
+	 *  类的作用：<br/> 
+	 *   
+	 *   
+	 * 
+	 * @author Administrator 
+	 * @version  
+	 * @since JDK 1.8
+	 */
+
+public class Test {
+	public static void main(String[] args) {
+		
+	}
+
+}
